35ffe9604dd569cd058d4504081dfbafd8afcda9
李进 <1320439118@qq.com>
2025-07-28 15:40:41 +0800

    update.

--- Chat_with_Datawhale_langchain/embedding/call_embedding.py
+++ Chat_with_Datawhale_langchain/embedding/call_embedding.py
@@ -1,11 +1,11 @@
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from Chat_with_Datawhale_langchain.embedding.zhipuai_embedding import ZhipuAIEmbeddings
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
+
 from Chat_with_Datawhale_langchain.llm.call_llm import parse_llm_api_key
+from langchain_rag_tutorial.embeddings import TongyiEmbeddings
+
 
 def get_embedding(embedding: str, embedding_key: str=None, env_file: str=None):
     if embedding == 'm3e':
@@ -14,7 +14,7 @@ def get_embedding(embedding: str, embedding_key: str=None, env_file: str=None):
         embedding_key = parse_llm_api_key(embedding)
     if embedding == "openai":
         return OpenAIEmbeddings(openai_api_key=embedding_key)
-    elif embedding == "zhipuai":
-        return ZhipuAIEmbeddings(zhipuai_api_key=embedding_key)
+    elif embedding == "tongyi":
+        return TongyiEmbeddings(dashscope_api_key=os.getenv("DASHSCOPE_API_KEY"))
     else:
         raise ValueError(f"embedding {embedding} not support ")
